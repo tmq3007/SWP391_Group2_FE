8c2c393fdf2c2dd58ba6861ec76eaf9fc1cd42f2
aronoob <39618559+mine1221@users.noreply.github.com>
2024-10-03 17:40:20 +0700

    Create admin dashboard

--- fe/src/component/Navbar/NavbarHomePage.jsx
+++ fe/src/component/Navbar/NavbarHomePage.jsx
@@ -99,6 +99,15 @@ export const NavbarHomePage = () => {
                             >
                                 <span style={{ color: "#FFFFFF" }}>Dash Board</span>
                             </Button>
+                        ) || userRole === "ROLE_ADMIN" && (
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                sx={{ backgroundColor: '#019376' }}
+                                onClick={() => navigate("/admin-dashboard")}
+                            >
+                                <span style={{ color: "#FFFFFF" }}>Dash Board</span>
+                            </Button>
                         )}
                     </>
                 ) : (
