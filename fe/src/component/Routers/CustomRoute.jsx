b55ce1dfa750179e6a343264c021e99ff480a9bb
tmq3007 <quantm3007@gmail.com>
2024-11-09 22:28:47 +0700

    commit

--- fe/src/component/Routers/CustomRoute.jsx
+++ fe/src/component/Routers/CustomRoute.jsx
@@ -154,18 +154,19 @@ const CustomRoute = () => {
                 <Route path="/help-center/*" element={<HelpingCenter/>}/>
                 <Route path="/my-profile/*" element={<CustomerProfile/>}/>
 
-                <Route path="/my-profile/orders"
-                       element={
-                           <ProtectedRoute role="ROLE_CUSTOMER">
-                               <CustomerProfile/>
-                           </ProtectedRoute>
-                       }/>
-                <Route path="/my-profile/wishlist"
-                       element={
-                           <ProtectedRoute role="ROLE_CUSTOMER">
-                               <CustomerProfile/>
-                           </ProtectedRoute>
-                       }/>
+                {/*<Route path="/my-profile/orders"*/}
+                {/*       element={*/}
+                {/*           <ProtectedRoute role="ROLE_CUSTOMER">*/}
+                {/*               <CustomerProfile/>*/}
+                {/*           </ProtectedRoute>*/}
+                {/*       }/>*/}
+                {/*<Route path="/my-profile/wishlist"*/}
+
+                {/*       element={*/}
+                {/*           <ProtectedRoute role="ROLE_CUSTOMER">*/}
+                {/*               <CustomerProfile/>*/}
+                {/*           </ProtectedRoute>*/}
+                {/*       }/>*/}
                 {/*<Route path="/payment-time-out" element={<PaymentTimeOut/>}/>*/}
                 {/*<Route*/}
                 {/*    path="/payment-time-out"*/}
