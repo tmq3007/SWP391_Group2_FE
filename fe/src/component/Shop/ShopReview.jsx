7c68413437b4fe2873085662dfbd881c1be2ecc3
tmq3007 <quantm3007@gmail.com>
2024-11-09 21:43:51 +0700

    commit

--- fe/src/component/Shop/ShopReview.jsx
+++ fe/src/component/Shop/ShopReview.jsx
@@ -69,49 +69,54 @@ export const ShopReview = () => {
     };
 
     return (
-        <div className="p-6 bg-white shadow-md rounded-lg">
+        <div className="p-6 bg-white shadow-md rounded-lg mt-5">
             <h2 className="text-xl font-semibold text-gray-800 mb-6">Reviews</h2>
-            <table className="min-w-full bg-white border border-gray-200">
-                <thead>
-                <tr className="bg-gray-50">
-                    <th className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase">ID</th>
-                    <th className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase">Product</th>
-                    <th className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase">Customer Review</th>
-                    <th
-                        className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase cursor-pointer"
-                        onClick={sortReviews}
-                    >
-                        Ratings {sortOrder === "asc" ? "↑" : "↓"}
-                    </th>
-                    <th className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase">Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                {review.map((re) => (
-                    <tr key={re.reviewId} className="hover:bg-gray-50">
-                        <td className="px-6 py-4 border-b text-sm font-medium text-gray-900">#ID: {re.reviewId}</td>
-                        <td className="px-6 py-4 border-b text-center">
-                            <img src={re.product.pictureUrl} alt={re.product.pictureUrl} className="h-10 w-10 rounded-full mx-auto" />
-                            <p>{re.product.productName}</p>
-                        </td>
-                        <td className="px-6 py-4 border-b text-sm text-gray-500">
-                            <p><span className="font-semibold">By {re.user.firstName + re.user.lastName}</span></p>
-                            <p>{re.reviewText}</p>
-                        </td>
-                        <td className="px-6 py-4 border-b text-center">
-                            <span className="text-green-500 font-semibold">{re.rating}</span>
-                        </td>
-                        <td className="px-6 py-4 border-b text-center">
-                            {new Date(re.createAt).toLocaleDateString('en-US', {
-                                year: 'numeric',
-                                month: 'long',
-                                day: 'numeric'
-                            })}
-                        </td>
+            <div className="overflow-y-auto max-h-[680px]">
+                <table className="min-w-full bg-white border border-gray-200 ">
+                    <thead>
+                    <tr className="bg-gray-50">
+                        <th className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase">ID</th>
+                        <th className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase">Product</th>
+                        <th className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase">Customer Review
+                        </th>
+                        <th
+                            className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase cursor-pointer"
+                            onClick={sortReviews}
+                        >
+                            Ratings {sortOrder === "asc" ? "↑" : "↓"}
+                        </th>
+                        <th className="px-6 py-3 border-b text-xs font-medium text-gray-500 uppercase">Date</th>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="">
+                    {review.map((re) => (
+                        <tr key={re.reviewId} className="hover:bg-gray-50 ">
+                            <td className="px-6 py-4 border-b text-sm font-medium text-gray-900">#ID: {re.reviewId}</td>
+                            <td className="px-6 py-4 border-b text-center">
+                                <img src={re.product.pictureUrl} alt={re.product.pictureUrl}
+                                     className="h-10 w-10 rounded-full mx-auto"/>
+                                <p>{re.product.productName}</p>
+                            </td>
+                            <td className="px-6 py-4 border-b text-sm text-gray-500">
+                                <p><span className="font-semibold">By {re.user.firstName + re.user.lastName}</span></p>
+                                <p>{re.reviewText}</p>
+                            </td>
+                            <td className="px-6 py-4 border-b text-center">
+                                <span className="text-green-500 font-semibold">{re.rating}</span>
+                            </td>
+                            <td className="px-6 py-4 border-b text-center">
+                                {new Date(re.createAt).toLocaleDateString('en-US', {
+                                    year: 'numeric',
+                                    month: 'long',
+                                    day: 'numeric'
+                                })}
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
